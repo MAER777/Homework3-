5f3012e41610bd5a6082dbc4835c810b999c4f28
dz <mark1000002@gmail.com>
2023-06-22 15:52:49 +0800

    1

---
+++ src/main/java/com/calculator/demo/Model/Faculty.java
@@ -0,0 +1,7 @@
+package com.calculator.demo.Model;
+
+public class Faculty {
+    private Long id;
+    private String name;
+    private int age;
+}
---
+++ src/main/java/com/calculator/demo/Model/Student.java
@@ -0,0 +1,7 @@
+package com.calculator.demo.Model;
+
+public class Student {
+    private Long id;
+    private String name;
+    private String color;
+}
